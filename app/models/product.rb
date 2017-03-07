c7bcfe3bb7395b4523923f2ef617c50bd7b793b7
Anne Brown <abbrownie@yahoo.com>
2017-03-07 16:31:29 +0000

    5.9 fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,7 @@ class Product < ActiveRecord::Base
   has_many :comments
   validates :name, presence: true
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
-end
+
 
 def highest_rating_comment
   comments.rating_desc.first
