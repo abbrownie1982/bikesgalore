51ec7b472744d7822029ead3348054f8842a0993
Anne Brown <abbrownie@yahoo.com>
2017-03-08 16:45:11 +0000

    5.11 automated email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
         :to => 'abbrownie@yahoo.com',
         :subject => "A new contact form message from #{name}")
   end
+  
+  def welcome(user)
+  @appname = "Bikes Galore"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+  end
 end
