ddf510da943bdfc511a27c6153089e54a36c0080
Anne Brown <abbrownie@yahoo.com>
2017-03-22 16:23:19 +0000

    6.13 fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,8 +8,8 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        # ActionCable.server.broadcast 'product_channel', comment: @comment
-        # ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: request.env['warden'].user })
+        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        #ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: request.env['warden'].user })
 
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
