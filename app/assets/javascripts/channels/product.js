74f44034200b5740ac6f901c314cf50910353757
Anne Brown <abbrownie@yahoo.com>
2017-04-11 17:38:23 +0000

    overall fixes

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,7 +1,7 @@
-App.product = App.cable.subscriptions.create({channel: "ProductChannel"}, {
+  App.product = App.cable.subscriptions.create("ProductChannel", {
   connected: function() {
     // Called when the subscription is ready for use on the server
-  },
+},
 
   disconnected: function() {
     // Called when the subscription has been terminated by the server
