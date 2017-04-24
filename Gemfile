3439a062e2dae84aa1f73e7820da0e574785331b
Anne Brown <abbrownie@yahoo.com>
2017-04-24 18:47:55 +0000

    working on failed app

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.0'
-gem 'ruby', '2.3.0'
+
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,22 +1,19 @@
 require 'rails_helper'
 
 describe Product do
-  context "when the product has comments" do
-    before do
-      @product = Product.create!(name: "race bike", price: 755, image_url: "bike6.jpg", colour: "black")
-      @user = User.create!(email: "anne@anne.com", password: "BikesGalore82!")
-      @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
-      @product.comments.create!(rating: 5, user: @user, body: "Fantastic bike!")
-      @product.comments.create!(rating: 3, user: @user, body: "Ok")
-    end
-
-    it "returns the average rating of all comments" do
-      expect(@product.average_rating).to eq 3
-    end
-
-    it "is not valid" do
-      expect(Product.new(description: "Nice bike")).not_to be_valid
-    end
+  before do
+    @product = Product.create!(name: "race bike", description: "Great bike")
+    @user = FactoryGirl.build(:user, email: "not_an_email")
+    @product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+    @product.comments.create!(rating: 3, user: @user, body: "Nice bike!")
+    @product.comments.create!(rating: 5, user: @user, body: "Amazing bike!")
   end
 
+  it "returns the average rating of all comments" do
+    expect(@product.average_rating).to eq 3
+  end
+
+  it "is not valid" do
+    expect(Product.new(description: "Nice Bike")).not_to be_valid
+  end
 end
